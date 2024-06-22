ad998964cd8b1f60dbb67a82e52446126ad06618
Will Cheng <98398911+weifish0@users.noreply.github.com>
2024-06-22 23:53:29 +0800

    javascript learning day 2

--- js-practice/index.js
+++ js-practice/index.js
@@ -7,3 +7,11 @@
 /*this is a comment*/
 
 document.getElementById("myP").textContent = "Test";
+
+let userName = "恍惚忽得鮪魚肚子";
+let subject = "Chemistry";
+let score = 0;
+
+
+console.log(`${userName} got ${score} in the final ${subject} test!`)
+console.log(typeof score);
