2ac5e63f73f70456da799490dfef5a5226c6afc1
Will Cheng <98398911+weifish0@users.noreply.github.com>
2024-07-03 23:50:11 +0800

    dice roller project

---
+++ js-practice/dice_roller_program/dice.js
@@ -0,0 +1,28 @@
+
+
+
+function rollDice(){
+    const diceTimes = document.getElementById("diceTimes");
+    const diceNumList = document.getElementById("diceNumList");
+    const alertMessage = document.getElementById("alertMessage");
+    const diceNumPic = document.getElementById("diceNumPic");
+    let diceTimesValue = Number(diceTimes.value);
+    let diceNums = [];
+    let images = [];
+
+    if(diceTimesValue > 0){
+        alertMessage.textContent = "";
+        for(let i = 0; i < diceTimesValue; i++){
+            let diceNum = Math.floor(Math.random() * 6) + 1;
+            diceNums.push(diceNum);
+            images.push(`<img src="./img/Dice-${diceNum}.png" alt="Dice-${diceNum}">`);
+        }
+        
+        diceNumList.textContent = `dice: ${diceNums.join(", ")}`;
+        diceNumPic.innerHTML = images.join("");
+    }
+    else{
+        alertMessage.textContent = "dice times should greater than 0";
+    }
+    
+}
